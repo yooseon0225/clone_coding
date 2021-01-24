49b3d0580a040a3e2b9ff3c406d55e7aebe1903e
yuseon <s20036@gsm.hs.kr>
2021-01-24 23:52:17 +0900

    main img

--- new_menu.js
+++ new_menu.js
@@ -7,17 +7,19 @@
 // $()함수는 괄호안에 CSS선택자를 정의해서 DOM노드(element)를 반환합니다. 하나의 함수로 여러가지의 종류의 객체를 생성할 수 있는 팩토리함수입니다.
 
 $(function(){
-    var $firstMenu = $('.menu-box > ul > li'),
-    $header = $('header')
+    var $firstMenu = $('menu-text'),
+    $header = $('header'),
     $headerHeight = $header.outerHeight();
 
     $firstMenu.mouseenter(function(){
-        var subHeight = $(this).find('ul').outerHeight();
-        $header.animate({height: $headerHeight + subHeight + 'px'},300);
+        var subHeight = $(this).find('ul').outerHeight(); 
+        //자식의 border의 높이를 구해옴
+        $header.stop().animate({height: $headerHeight + subHeight + 'px'},300);
+        //원래의 header 높이와 구해온 menu의 높이를 더해서 0.3초에 걸쳐 나오게 함
     })
     .mouseleave(function(){
-        $header.animate({height: $headerHeight + 'px'},300);
-    })
+        $header.stop().animate({height: $headerHeight + 'px'},300);
+    });
      
 });
 
