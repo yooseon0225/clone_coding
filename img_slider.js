d142ccb880d6efd972a1857d974430917523ddeb
yuseon <s20036@gsm.hs.kr>
2021-01-28 00:27:42 +0900

    img change menu fix

--- img_slider.js
+++ img_slider.js
@@ -31,3 +31,29 @@ function showImg() {
 
     setTimeout("showImg()", 7000);
 }
+
+function changeMainImg(idx) {
+    if(idx == 1) {
+        var imgTag = document.getElementById("photo");
+        imgTag.setAttribute("src", "images/main_img01.jpg");    
+    }
+    else if(idx == 2) {
+        var imgTag = document.getElementById("photo");
+        imgTag.setAttribute("src", "images/main_img02.jpg");  
+    }
+    else if(idx == 3) {
+        var imgTag = document.getElementById("photo");
+        imgTag.setAttribute("src", "images/main_img03.jpg");  
+    }
+    else if(idx == 4) {
+        var imgTag = document.getElementById("photo");
+        imgTag.setAttribute("src", "images/main_img04.jpg");  
+    }
+}
+
+
+// 수정할 부분
+// Math random 함수를 사용해서 일정하게 나오는 것이 아니라 1-2-3-4 순서가 아니라 랜덤으로 나옴 (animate 사용해서 수정 필요)
+// border-bottom 부분 부드럽게 나오도록 수정
+// 사진 부드럽게 넘어가기 만들기
+// 버튼 hover 이 아니라 버튼 클릭 시 img-change-menu의 색상을 변경하도록 수정
