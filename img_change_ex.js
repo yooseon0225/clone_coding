ab07c53ffb4270fee363f6d4b36f1213be28bbcb
yuseon <s20036@gsm.hs.kr>
2021-02-01 23:45:33 +0900

    fix img change

--- img_change_ex.js
+++ img_change_ex.js
@@ -1,40 +1,13 @@
+var now_img;
+var last_img;
 
-var slideWrapper = document.querySelector('.container');
-var slides = document.querySelectorAll('.item');
-var totalSlides = slides.length; // item의 갯수
-
-
-var sliderWidth = slideWrapper.clientWidth; // container의 width
-var slideIndex = 0;
-var slider = document.querySelector('.slider');
-
-slider.style.width = sliderWidth * totalSlides + 'px';
-
-
-function showSlides(n) {
-    slideIndex = n;
-    if (slideIndex == -1) {
-        slideIndex = totalSlides - 1;
-    } else if (slideIndex === totalSlides) {
-        slideIndex = 0;
-    }
-    slider.style.left = -(sliderWidth * slideIndex) + 'px';
+window.onload = function(){
+    setInterval("fn_change()",2000);
 }
 
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
-  
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+function fn_change(){
+    now_img = $("img").eq(0);
+    last_img = $("img").eq(-1);
 
-var nextBtn = document.querySelector('.next');
-var prevBtn = document.querySelector('.prev');
-
-nextBtn.addEventListener('click', function () {
-    plusSlides(1);
-});
-prevBtn.addEventListener('click', function () {
-    plusSlides(-1);
-});
+    last_img.addClass("z-idx").css("opacity",0)
+}
